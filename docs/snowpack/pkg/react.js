3f0e43876faed71dcf0579ec7de4479cc035d8d4
Andres Flores <andresflo883@gmail.com>
2022-12-24 17:37:53 -0500

    edited js src

--- docs/snowpack/pkg/react.js
+++
@@ -1,8 +0,0 @@
-import { r as react } from './common/index-210ebed7.js';
-export { r as default } from './common/index-210ebed7.js';
-
-
-
-var useEffect = react.useEffect;
-var useState = react.useState;
-export { useEffect, useState };
